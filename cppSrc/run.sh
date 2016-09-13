c6c63c8efa0e836958d51d7f12022f2c4ce4aea7
tvoicu91 <tudor.wadv@gmail.com>
2016-09-13 19:03:53 +0200

    Updated license with TU Delft.
    Former-commit-id: f3a92e1bd12397d41b341c889a7330039700d321

--- cppSrc/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ${SPARK_HOME}
-./bin/spark-submit --driver-java-options "-XX:+ShowMessageBoxOnError" --class org.tudelft.ewi.ceng.examples.pairHMM.PairHmmMain ${SPARK_JNI}/target/jni-spark-0.1.jar $1 $2 ${JAVA_HOME} FPGA $3 --conf "spark.executor.memory=4g spark.driver.maxResultSize=4g spark.driver.memory=4g"
