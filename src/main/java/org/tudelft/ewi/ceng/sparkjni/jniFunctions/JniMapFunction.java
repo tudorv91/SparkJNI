f3a92e1bd12397d41b341c889a7330039700d321
tvoicu91 <tudor.wadv@gmail.com>
2016-09-13 19:03:53 +0200

    Updated license with TU Delft.

--- src/main/java/org/tudelft/ewi/ceng/sparkjni/jniFunctions/JniMapFunction.java
+++ src/main/java/org/tudelft/ewi/ceng/sparkjni/jniFunctions/JniMapFunction.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 Tudor Alexandru Voicu
+ * Copyright 2016 Tudor Alexandru Voicu and Zaid Al-Ars, TUDelft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
