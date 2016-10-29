a5ebeab3267588cb48616c1ed464f30e86b7b4c6
Tudor Voicu <tudor.wadv@gmail.com>
2016-10-29 13:52:44 +0200

    Pi OpenCL example added

---
+++ src/main/java/org/heterojni/examples/sparkJNIPi/SumArray.java
@@ -0,0 +1,7 @@
+package org.heterojni.examples.sparkJNIPi;
+
+/**
+ * Created by tudor on 29-10-16.
+ */
+public class SumArray {
+}
---
+++ src/main/java/org/heterojni/sparkjni/utils/DeployTimesLogger.java
@@ -0,0 +1,9 @@
+package org.heterojni.sparkjni;
+
+public class DeployTimesLogger {
+    protected long start = 0L;
+    protected long genTime = 0L;
+    protected long buildTime = 0L;
+    protected long libLoadTime = 0L;
+    protected long javahTime = 0L;
+}
